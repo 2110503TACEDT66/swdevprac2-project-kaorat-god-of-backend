b45f3db6e28ce4b4c21a186b69a06326c8b44aa7
Auiz <nopphawit47374@gmail.com>
2024-03-25 22:11:35 +0700

    fix code

--- src/libs/getMassageshop.tsx
+++ src/libs/getMassageshop.tsx
@@ -1,5 +1,5 @@
 export default async function getMassageshop(id: string) {
-    const response = await fetch(`https://vaccine-app-backend.vercel.app/api/v1/massageshops/${id}`)
+    const response = await fetch(`${process.env.BACKEND_URL}/api/v1/massageshops/${id}`)
     
     if (!response.ok) {
         throw new Error('Failed to fetch massageshop')
