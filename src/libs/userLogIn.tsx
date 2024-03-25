c840c8a62f36b1250c1627e72496edfcc3cd4095
Auiz <nopphawit47374@gmail.com>
2024-03-25 22:14:07 +0700

    fix code

--- src/libs/userLogIn.tsx
+++ src/libs/userLogIn.tsx
@@ -1,6 +1,6 @@
 export default async function userLogIn(userEmail: string, userPassword: string) {
     
-    const response = await fetch('https://vaccine-app-backend.vercel.app/api/v1/auth/login', {
+    const response = await fetch(`${process.env.BACKEND_URL}/api/v1/auth/login`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
